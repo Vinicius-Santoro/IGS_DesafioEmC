df9e81f1b456196c33c9e20a474f31c36c4d7cd2
vnazioze <vinicius.rio@ifsp.edu.com.br>
2022-07-28 21:44:07 -0300

    feat: :rocket: adicionando base de tasks

--- Desafio4.c
+++ Desafio4.c
@@ -9,17 +9,26 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+
 int calcular_maximum_bitwise(int v1, int v2) {
-//Complete.
+
+    /*
+    1: transformar v1 em binário de 8 bytes;
+    2: transformar v2 em binário de 8 bytes;
+    3: comparar por binario
+        - transformar para decimal e validar o maior
+    */
+
 
 }
 int main() {
- int valor_1, valor_2, maior_valor;
- printf("Informe os valores.\n");
- scanf("%d %d", &valor_1, &valor_2);
- maior_valor = calculate_the_maximum(valor_1, valor_2);
 
- printf("O maior valor e: %d .\n", maior_valor);
+    int valor_1, valor_2, maior_valor;
+    printf("Informe os valores.\n");
+    scanf("%d %d", &valor_1, &valor_2);
+    maior_valor = calcular_maximum_bitwise(valor_1, valor_2);
+
+    printf("O maior valor e: %d .\n", maior_valor);
 
  return 0;
 }
